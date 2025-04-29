fd627f846827e7f79c810805904fab40e4d0c054
Ahmed Assem <ahmedassem2610@gmail.com>
2025-04-29 14:52:38 +0300

    create home.js & helpers.js & export showCards func

--- js/carListings.js
+++ js/carListings.js
@@ -1,4 +1,5 @@
 import Cars from "./classes/Cars.js";
+import showCards from "./helpers.js";
 
 const carsClass = new Cars();
 await carsClass.ready;
@@ -29,7 +30,7 @@ types.forEach((type) => {
   typesFilter.appendChild(option);
 });
 
-showCards(cars);
+showCards(cars, carsContainer);
 
 //Filter Functionality
 
@@ -59,7 +60,7 @@ document.getElementById("filter-form").addEventListener("submit", (e) => {
     brand,
   });
 
-  showCards(filteredCars);
+  showCards(filteredCars, carsContainer);
 });
 
 //Reset
@@ -70,7 +71,7 @@ document.getElementById("reset-filter").addEventListener("click", () => {
   maxPriceFilter.value = undefined;
   availableFilter.checked = false;
 
-  showCards(cars);
+  showCards(cars, carsContainer);
 });
 
 //Search Functionality
@@ -78,64 +79,5 @@ searchInput.addEventListener("input", () => {
   const searchText = searchInput.value;
   const searchedCars = carsClass.searchCars(searchText);
 
-  showCards(searchedCars);
+  showCards(searchedCars, carsContainer);
 });
-
-//////////////////////////////
-
-function showCards(cars) {
-  carsContainer.innerHTML = "";
-  if (cars.length === 0) {
-    carsContainer.innerHTML = `
-    <div id="noCars">
-  <h2>No cars matched your search.</h2>
-  <p>Try adjusting your search/filter for better results!</p>
-  <img
-    src="../assets/noCar.svg"
-    alt="no cars"
-    width="150"
-    height="150"
-  />
-  </div>
-  `;
-  }
-
-  cars.forEach((car) => {
-    const col = document.createElement("div");
-    col.className = " col-md-6 col-lg-4";
-
-    let availabilitySpan =
-      car.available === true
-        ? `<span id="available"><i class="bi bi-check-circle me-1"></i> Available</span>`
-        : `<span id="booked"><i class="bi bi-x-circle"></i> Booked</span>`;
-
-    col.innerHTML = `
-        <div class="car-card">
-          <img src="${car.images[0]}" alt="${car.brand} ${car.model}">
-          ${availabilitySpan}
-          <div class="car-card-body">
-            <div class="d-flex justify-content-between align-items-center mb-2">
-              <h5 class="car-card-title mb-0">${car.brand}</h5>
-              <span class="car-card-price">$${
-                car.pricePerDay
-              } <small class="text-muted">/ day</small></span>
-            </div>
-            <p class="text-muted mb-3">${car.type}</p>
-    
-            <div class="car-card-features">
-              ${car.features
-                .map(
-                  (f) =>
-                    `<div><i class="bi bi-check-circle me-1"></i> ${f}</div>`
-                )
-                .join("")}
-            </div>
-    
-            <a href="carDetails.html?car_id=${car.id}" class="car-card-btn">View Details</a>
-          </div>
-        </div>
-      `;
-
-    carsContainer.appendChild(col);
-  });
-}
--- js/home.js
+++ js/home.js
@@ -1,10 +1,13 @@
 import Cars from "./classes/Cars.js";
 import Users from "./classes/Users.js";
+import showCards from "./helpers.js";
 
 const cars = new Cars();
 await cars.ready;
-// console.log(cars.getAllCars());
 
 const users = new Users();
 await users.ready;
-// console.log(users.getUsers());
+
+const carsContainer = document.getElementById("car-cards-container");
+
+showCards(cars.getAllCars().slice(0, 6), carsContainer);
